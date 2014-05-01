7e5b93cacf1b77c57ca4831e4ad5d0ed38b694ae
Bea Lam <bea.lam@jollamobile.com>
2014-05-01 16:16:18 +1000

    [sociald] Initial VK support including VK Posts sync adaptor. Contributes to JB#19270
    This commit adds VK as a supported social network in sociald, and
    adds a posts sync adaptor.
    
    Contributes to JB#19270

---
+++ src/vk/vk-posts/vkpostsyncadaptor.cpp
@@ -0,0 +1,227 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2013 Jolla Ltd.
+ ** Contact: Bea Lam <bea.lam@jollamobile.com>
+ **
+ ****************************************************************************/
+
+#include "vkpostsyncadaptor.h"
+#include "trace.h"
+#include "constants_p.h"
+
+#include <QtCore/QPair>
+#include <QtCore/QJsonArray>
+#include <QtCore/QJsonValue>
+#include <QtCore/QJsonObject>
+#include <QtCore/QJsonDocument>
+#include <QtCore/QUrlQuery>
+
+#define SOCIALD_VK_POSTS_ID_PREFIX QStringLiteral("vk-posts-")
+#define SOCIALD_VK_POSTS_GROUPNAME QStringLiteral("vk")
+
+VKPostSyncAdaptor::VKPostSyncAdaptor(QObject *parent)
+    : VKDataTypeSyncAdaptor(SocialNetworkSyncAdaptor::Posts, parent)
+{
+    setInitialActive(m_db.isValid());
+}
+
+VKPostSyncAdaptor::~VKPostSyncAdaptor()
+{
+}
+
+QString VKPostSyncAdaptor::syncServiceName() const
+{
+    return QStringLiteral("vk-microblog");
+}
+
+void VKPostSyncAdaptor::sync(const QString &dataTypeString, int accountId)
+{
+    // call superclass impl.
+    VKDataTypeSyncAdaptor::sync(dataTypeString, accountId);
+}
+
+void VKPostSyncAdaptor::purgeDataForOldAccounts(const QList<int> &purgeIds)
+{
+    Q_UNUSED(purgeIds);
+    if (purgeIds.size()) {
+        foreach (int accountIdentifier, purgeIds) {
+            m_db.removePosts(accountIdentifier);
+        }
+        m_db.commit();
+        m_db.wait();
+    }
+}
+
+void VKPostSyncAdaptor::beginSync(int accountId, const QString &accessToken)
+{
+    m_db.removePosts(accountId); // always purge all posts for the account, prior to syncing most recent.
+    requestPosts(accountId, accessToken);
+}
+
+void VKPostSyncAdaptor::finalize(int accountId)
+{
+    Q_UNUSED(accountId)
+    m_db.commit();
+    m_db.wait();
+}
+
+void VKPostSyncAdaptor::requestPosts(int accountId, const QString &accessToken)
+{
+    QList<QPair<QString, QString> > queryItems;
+    queryItems.append(QPair<QString, QString>(QString(QStringLiteral("access_token")), accessToken));
+    queryItems.append(QPair<QString, QString>(QString(QStringLiteral("extended")), QStringLiteral("1")));
+    queryItems.append(QPair<QString, QString>(QString(QLatin1String("v")), QStringLiteral("5.21"))); // version
+
+    QUrl url(QStringLiteral("https://api.vk.com/method/wall.get"));
+    QUrlQuery query(url);
+    query.setQueryItems(queryItems);
+    url.setQuery(query);
+    QNetworkReply *reply = networkAccessManager->get(QNetworkRequest(url));
+    
+    if (reply) {
+        reply->setProperty("accountId", accountId);
+        reply->setProperty("accessToken", accessToken);
+        connect(reply, SIGNAL(error(QNetworkReply::NetworkError)), this, SLOT(errorHandler(QNetworkReply::NetworkError)));
+        connect(reply, SIGNAL(sslErrors(QList<QSslError>)), this, SLOT(sslErrorsHandler(QList<QSslError>)));
+        connect(reply, SIGNAL(finished()), this, SLOT(finishedPostsHandler()));
+
+        // we're requesting data.  Increment the semaphore so that we know we're still busy.
+        incrementSemaphore(accountId);
+        setupReplyTimeout(accountId, reply);
+    } else {
+        TRACE(SOCIALD_ERROR,
+                QString(QStringLiteral("error: unable to request home posts from VK account with id %1"))
+                .arg(accountId));
+    }
+}
+
+void VKPostSyncAdaptor::finishedPostsHandler()
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(sender());
+    bool isError = reply->property("isError").toBool();
+    int accountId = reply->property("accountId").toInt();
+
+    QByteArray replyData = reply->readAll();
+    disconnect(reply);
+    reply->deleteLater();
+    removeReplyTimeout(accountId, reply);
+
+    bool ok = false;
+    QJsonObject parsed = parseJsonObjectReplyData(replyData, &ok);
+
+    if (!isError && ok && parsed.contains(QStringLiteral("response"))) {
+        QJsonObject responseObj = parsed.value(QStringLiteral("response")).toObject();
+        QJsonArray items = responseObj.value(QStringLiteral("items")).toArray();
+        if (!items.size()) {
+            TRACE(SOCIALD_DEBUG,
+                    QString(QLatin1String("no feed posts received for account %1"))
+                    .arg(accountId));
+            decrementSemaphore(accountId);
+            return;
+        }
+
+        QJsonArray profileValues = responseObj.value(QStringLiteral("profiles")).toArray();
+        QList<UserProfile> userProfiles;
+        foreach (const QJsonValue &entry, profileValues) {
+            UserProfile profile = UserProfile::fromJsonObject(entry.toObject());
+            userProfiles << profile;
+        }
+
+        foreach (const QJsonValue &entry, items) {
+            QJsonObject object = entry.toObject();
+            if (!object.isEmpty()) {
+                saveVKPostFromObject(accountId, object, userProfiles);
+            }
+        }
+    } else {
+        // error occurred during request.
+        TRACE(SOCIALD_ERROR,
+                QString(QStringLiteral("error: unable to parse event feed data from request with account %1; got: %2"))
+                .arg(accountId).arg(QString::fromLatin1(replyData.constData())));
+    }
+
+    // we're finished this request.  Decrement our busy semaphore.
+    decrementSemaphore(accountId);
+}
+
+void VKPostSyncAdaptor::saveVKPostFromObject(int accountId, const QJsonObject &post, const QList<UserProfile> &userProfiles)
+{
+    VKPostsDatabase::Post newPost;
+    newPost.fromId = int(post.value(QStringLiteral("from_id")).toDouble());
+    newPost.toId = int(post.value(QStringLiteral("to_id")).toDouble());
+    newPost.postType = post.value(QStringLiteral("post_type")).toString();
+    newPost.replyOwnerId = int(post.value(QStringLiteral("reply_owner_id")).toDouble());
+    newPost.replyPostId = int(post.value(QStringLiteral("reply_post_id")).toDouble());
+    newPost.signerId = int(post.value(QStringLiteral("signer_id")).toDouble());
+    newPost.friendsOnly = int(post.value(QStringLiteral("friends_only")).toDouble()) == 1;
+
+    QJsonObject commentInfo = post.value(QStringLiteral("comments")).toObject();
+    VKPostsDatabase::Comments comments;
+    comments.count = int(commentInfo.value(QStringLiteral("count")).toDouble());
+    comments.userCanComment = int(commentInfo.value(QStringLiteral("count")).toDouble()) == 1;
+    newPost.comments = comments;
+
+    QJsonObject likeInfo = post.value(QStringLiteral("likes")).toObject();
+    VKPostsDatabase::Likes likes;
+    likes.count = int(likeInfo.value(QStringLiteral("count")).toDouble());
+    likes.userLikes = int(likeInfo.value(QStringLiteral("user_likes")).toDouble()) == 1;
+    likes.userCanLike = int(likeInfo.value(QStringLiteral("can_like")).toDouble()) == 1;
+    likes.userCanPublish = int(likeInfo.value(QStringLiteral("can_publish")).toDouble()) == 1;
+    newPost.likes = likes;
+
+    QJsonObject repostInfo = post.value(QStringLiteral("reposts")).toObject();
+    VKPostsDatabase::Reposts reposts;
+    reposts.count = int(repostInfo.value(QStringLiteral("count")).toDouble());
+    reposts.userReposted = int(repostInfo.value(QStringLiteral("user_reposted")).toDouble()) == 1;
+    newPost.reposts = reposts;
+
+    QJsonObject postSourceInfo = post.value(QStringLiteral("post_source")).toObject();
+    VKPostsDatabase::PostSource postSource;
+    postSource.type = postSourceInfo.value(QStringLiteral("type")).toString();
+    postSource.data = postSourceInfo.value(QStringLiteral("data")).toString();
+    newPost.postSource = postSource;
+
+    QList<QPair<QString, SocialPostImage::ImageType> > images;
+    QJsonArray attachmentsInfo = post.value(QStringLiteral("attachments")).toArray();
+    Q_FOREACH (const QJsonValue &attValue, attachmentsInfo) {
+        QJsonObject attObject = attValue.toObject();
+        QString type = attObject.value(QStringLiteral("type")).toString();
+        QJsonObject attValue = attObject.value(type).toObject();
+        if (type == QStringLiteral("photo")
+                || type == QStringLiteral("posted_photo")
+                || type == QStringLiteral("graffiti")) {
+            QString src = attValue.value(QStringLiteral("photo_75")).toString();
+            if (!src.isEmpty()) {
+                images.append(qMakePair(src, SocialPostImage::Photo));
+            }
+        }
+    }
+
+    VKPostsDatabase::GeoLocation geo;
+    QJsonObject geoInfo = post.value(QStringLiteral("geo")).toObject();
+    if (!geoInfo.isEmpty()) {
+        geo.placeId = int(geoInfo.value(QStringLiteral("place_id")).toDouble());
+        geo.title = geoInfo.value(QStringLiteral("title")).toString();
+        geo.type = geoInfo.value(QStringLiteral("type")).toString();
+        geo.countryId = int(geoInfo.value(QStringLiteral("country_id")).toDouble());
+        geo.cityId = int(geoInfo.value(QStringLiteral("city_id")).toDouble());
+        geo.address = geoInfo.value(QStringLiteral("address")).toString();
+        geo.showMap = int(geoInfo.value(QStringLiteral("showmap")).toDouble()) == 1;   // type????
+        newPost.geo = geo;
+    }
+
+    VKPostsDatabase::CopyPost copyPost;
+    copyPost.createdTime = VKDataTypeSyncAdaptor::parseVKDateTime(post.value(QStringLiteral("copy_post_date")));
+    copyPost.type = post.value(QStringLiteral("copy_post_type")).toString();
+    copyPost.ownerId = int(post.value(QStringLiteral("copy_owner_id")).toDouble());
+    copyPost.postId = int(post.value(QStringLiteral("copy_post_id")).toDouble());
+    copyPost.text = post.value(QStringLiteral("copy_text")).toString();
+    newPost.copyPost = copyPost;
+
+    const UserProfile &user(VKDataTypeSyncAdaptor::findProfile(userProfiles, newPost.fromId));
+    QString identifier = QString::number(post.value(QStringLiteral("id")).toDouble());
+    QDateTime createdTime = VKDataTypeSyncAdaptor::parseVKDateTime(post.value(QStringLiteral("date")));
+    QString body = post.value(QStringLiteral("text")).toString();
+
+    m_db.addVKPost(identifier, createdTime, body, newPost, images, user.name(), user.icon, accountId);
+}
---
+++ src/vk/vkdatatypesyncadaptor.cpp
@@ -0,0 +1,378 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2013 Jolla Ltd.
+ ** Contact: Chris Adams <chris.adams@jollamobile.com>
+ **
+ ****************************************************************************/
+
+#include "vkdatatypesyncadaptor.h"
+#include "trace.h"
+
+#include <QtCore/QVariantMap>
+#include <QtCore/QObject>
+#include <QtCore/QList>
+#include <QtCore/QString>
+#include <QtCore/QByteArray>
+
+//libsailfishkeyprovider
+#include <sailfishkeyprovider.h>
+
+//libaccounts-qt
+#include <Accounts/Manager>
+#include <Accounts/Account>
+#include <Accounts/AccountService>
+#include <Accounts/Service>
+
+//libsignon-qt
+#include <SignOn/SessionData>
+#include <SignOn/Identity>
+
+VKDataTypeSyncAdaptor::UserProfile::UserProfile()
+{
+}
+
+VKDataTypeSyncAdaptor::UserProfile::~UserProfile()
+{
+}
+
+VKDataTypeSyncAdaptor::UserProfile::UserProfile(const UserProfile &other)
+{
+    operator=(other);
+}
+
+VKDataTypeSyncAdaptor::UserProfile &VKDataTypeSyncAdaptor::UserProfile::operator=(const UserProfile &other)
+{
+    if (&other == this) {
+        return *this;
+    }
+    uid = other.uid;
+    firstName = other.firstName;
+    lastName = other.lastName;
+    icon = other.icon;
+    return *this;
+}
+
+VKDataTypeSyncAdaptor::UserProfile VKDataTypeSyncAdaptor::UserProfile::fromJsonObject(const QJsonObject &object)
+{
+    UserProfile user;
+    user.uid = int(object.value(QStringLiteral("id")).toDouble());
+    user.firstName = object.value(QStringLiteral("first_name")).toString();
+    user.lastName = object.value(QStringLiteral("last_name")).toString();
+    user.icon = object.value(QStringLiteral("photo_50")).toString();
+    return user;
+}
+
+QString VKDataTypeSyncAdaptor::UserProfile::name() const
+{
+    // TODO locale-specific joining of names
+    QString personName;
+    if (!firstName.isEmpty()) {
+        personName += firstName;
+    }
+    if (!lastName.isEmpty()) {
+        if (!firstName.isEmpty()) {
+            personName += ' ';
+        }
+        personName += lastName;
+    }
+    return personName;
+}
+
+QDateTime VKDataTypeSyncAdaptor::parseVKDateTime(const QJsonValue &v)
+{
+    if (v.type() != QJsonValue::Double) {
+        return QDateTime();
+    }
+    int t = int(v.toDouble());
+    return QDateTime::fromTime_t(t);
+}
+
+VKDataTypeSyncAdaptor::UserProfile VKDataTypeSyncAdaptor::findProfile(const QList<UserProfile> &profiles, int uid)
+{
+    Q_FOREACH (const UserProfile &user, profiles) {
+        if (user.uid == uid) {
+            return user;
+        }
+    }
+    return UserProfile();
+}
+
+
+VKDataTypeSyncAdaptor::VKDataTypeSyncAdaptor(SocialNetworkSyncAdaptor::DataType dataType, QObject *parent)
+    : SocialNetworkSyncAdaptor("vk", dataType, parent), m_triedLoading(false)
+{
+}
+
+VKDataTypeSyncAdaptor::~VKDataTypeSyncAdaptor()
+{
+}
+
+void VKDataTypeSyncAdaptor::sync(const QString &dataTypeString, int accountId)
+{
+    if (dataTypeString != SocialNetworkSyncAdaptor::dataTypeName(dataType)) {
+        TRACE(SOCIALD_ERROR,
+                QString(QLatin1String("error: VK %1 sync adaptor was asked to sync %2"))
+                .arg(SocialNetworkSyncAdaptor::dataTypeName(dataType)).arg(dataTypeString));
+        setStatus(SocialNetworkSyncAdaptor::Error);
+        return;
+    }
+
+    if (clientId().isEmpty()) {
+        TRACE(SOCIALD_ERROR, QString(QLatin1String("error: client id couldn't be retrieved for VK")));
+        setStatus(SocialNetworkSyncAdaptor::Error);
+        return;
+    }
+
+    // either a single-account sync, or an all-account sync.
+    // an all-account sync is a three stage process.
+    // 1) if an account has been removed, we need to purge the data we retrieved with it
+    // 2) if an account has been added, we need to pull data for the account
+    // 3) for existing accounts, pull new data for the existing account
+    setStatus(SocialNetworkSyncAdaptor::Busy);
+
+    QList<int> newIds, purgeIds, updateIds;
+    if (accountId == 0) {
+        // all account sync.  determine accounts added/removed/need updating.
+        checkAccounts(dataType, &newIds, &purgeIds, &updateIds);
+
+        // We only actually perform the purge operation for all-account (template) syncs.
+        purgeDataForOldAccounts(purgeIds); // call the derived-class purge entrypoint.
+
+        TRACE(SOCIALD_DEBUG,
+                QString(QLatin1String("successfully triggered sync of %1: purged %2 accounts"))
+                .arg(SocialNetworkSyncAdaptor::dataTypeName(dataType)).arg(purgeIds.size()));
+
+        setFinishedInactive(); // just had to purge, and we're done.
+    } else {
+        // single account sync.
+        updateDataForAccounts(QList<int>() << accountId);
+
+        TRACE(SOCIALD_DEBUG,
+                QString(QLatin1String("successfully triggered sync with profile: %1"))
+                .arg(m_accountSyncProfile->name()));
+    }
+}
+
+void VKDataTypeSyncAdaptor::updateDataForAccounts(const QList<int> &accountIds)
+{
+    if (accountIds.size() != 1) {
+        // Since the "split monolithic plugin" refactoring, this function
+        // should only ever be called for a single account.
+        // TODO: refactor all of the plugins even more completely, to
+        // remove the per-accountId state data (maps etc) and "fix" the
+        // function signatures to match the new per-account paradigm.
+        qWarning() << Q_FUNC_INFO << "called with multiple accounts - ERROR!";
+        setStatus(SocialNetworkSyncAdaptor::Error);
+        return;
+    }
+
+    foreach (int accountId, accountIds) {
+        // will be decremented by either signOnError or signOnResponse.
+        // we increment them prior to the loop below to avoid spurious
+        // "all are zero" causing setFinishedInactive() too early,
+        // if one of the accounts could not be loaded.
+        incrementSemaphore(accountId);
+    }
+
+    foreach (int accountId, accountIds) {
+        Accounts::Account *account = accountManager->account(accountId);
+        if (!account) {
+            TRACE(SOCIALD_ERROR,
+                  QString(QLatin1String("error: existing account with id %1 couldn't be retrieved"))
+                  .arg(accountId));
+            setStatus(SocialNetworkSyncAdaptor::Error);
+            decrementSemaphore(accountId);
+            continue;
+        }
+        signIn(account);
+    }
+}
+
+
+void VKDataTypeSyncAdaptor::errorHandler(QNetworkReply::NetworkError err)
+{
+    QNetworkReply *reply = qobject_cast<QNetworkReply*>(sender());
+    QByteArray replyData = reply->readAll();
+    int accountId = reply->property("accountId").toInt();
+
+    TRACE(SOCIALD_ERROR,
+            QString(QLatin1String("error: %1 request with account %2 experienced error: %3"))
+            .arg(SocialNetworkSyncAdaptor::dataTypeName(dataType)).arg(accountId).arg(err));
+    // set "isError" on the reply so that adapters know to ignore the result in the finished() handler
+    reply->setProperty("isError", QVariant::fromValue<bool>(true));
+    // Note: not all errors are "unrecoverable" errors, so we don't change the status here.
+
+    bool ok = false;
+    QJsonObject parsed = parseJsonObjectReplyData(replyData, &ok);
+    if (ok && parsed.contains(QLatin1String("error"))) {
+        QJsonObject errorReply = parsed.value("error").toObject();
+        // Password Changed on server side
+        if (errorReply.value("code").toDouble() == 190 &&
+                errorReply.value("error_subcode").toDouble() == 460) {
+            int accountId = reply->property("accountId").toInt();
+            Accounts::Account *account = accountManager->account(accountId);
+            if (account) {
+                setCredentialsNeedUpdate(account);
+            }
+        }
+    }
+}
+
+void VKDataTypeSyncAdaptor::sslErrorsHandler(const QList<QSslError> &errs)
+{
+    QString sslerrs;
+    foreach (const QSslError &e, errs) {
+        sslerrs += e.errorString() + "; ";
+    }
+    if (errs.size() > 0) {
+        sslerrs.chop(2);
+    }
+    TRACE(SOCIALD_ERROR,
+            QString(QLatin1String("error: %1 request with account %2 experienced ssl errors: %3"))
+            .arg(SocialNetworkSyncAdaptor::dataTypeName(dataType)).arg(sender()->property("accountId").toInt()).arg(sslerrs));
+    // set "isError" on the reply so that adapters know to ignore the result in the finished() handler
+    sender()->setProperty("isError", QVariant::fromValue<bool>(true));
+    // Note: not all errors are "unrecoverable" errors, so we don't change the status here.
+}
+
+QString VKDataTypeSyncAdaptor::clientId()
+{
+    if (!m_triedLoading) {
+        loadClientId();
+    }
+    return m_clientId;
+}
+
+void VKDataTypeSyncAdaptor::loadClientId()
+{
+    m_triedLoading = true;
+    char *cClientId = NULL;
+    int cSuccess = SailfishKeyProvider_storedKey("vk", "vk-sync", "client_id", &cClientId);
+    if (cSuccess != 0 || cClientId == NULL) {
+        return;
+    }
+
+    m_clientId = QLatin1String(cClientId);
+    free(cClientId);
+    return;
+}
+
+void VKDataTypeSyncAdaptor::setCredentialsNeedUpdate(Accounts::Account *account)
+{
+    qWarning() << "sociald:VKontakte: setting CredentialsNeedUpdate to true for account:" << account->id();
+    Accounts::Service srv(accountManager->service(syncServiceName()));
+    account->selectService(srv);
+    account->setValue(QStringLiteral("CredentialsNeedUpdate"), QVariant::fromValue<bool>(true));
+    account->setValue(QStringLiteral("CredentialsNeedUpdateFrom"), QVariant::fromValue<QString>(QString::fromLatin1("sociald-vkontakte")));
+    account->selectService(Accounts::Service());
+    account->syncAndBlock();
+}
+
+void VKDataTypeSyncAdaptor::signIn(Accounts::Account *account)
+{
+    // Fetch clientId from keyprovider
+    int accountId = account->id();
+    if (!checkAccount(account) || clientId().isEmpty()) {
+        decrementSemaphore(accountId);
+        return;
+    }
+
+    // grab out a valid identity for the sync service.
+    Accounts::Service srv(accountManager->service(syncServiceName()));
+    account->selectService(srv);
+    SignOn::Identity *identity = account->credentialsId() > 0 ? SignOn::Identity::existingIdentity(account->credentialsId()) : 0;
+    if (!identity) {
+        TRACE(SOCIALD_ERROR,
+                QString(QLatin1String("error: account %1 has no valid credentials, cannot sign in"))
+                .arg(accountId));
+        decrementSemaphore(accountId);
+        return;
+    }
+
+    Accounts::AccountService accSrv(account, srv);
+    QString method = accSrv.authData().method();
+    QString mechanism = accSrv.authData().mechanism();
+    SignOn::AuthSession *session = identity->createSession(method);
+    if (!session) {
+        TRACE(SOCIALD_ERROR,
+                QString(QLatin1String("error: could not create signon session for account %1"))
+                .arg(accountId));
+        identity->deleteLater();
+        decrementSemaphore(accountId);
+        return;
+    }
+
+    QVariantMap signonSessionData = accSrv.authData().parameters();
+    signonSessionData.insert("ClientId", clientId());
+    signonSessionData.insert("UiPolicy", SignOn::NoUserInteractionPolicy);
+
+    connect(session, SIGNAL(response(SignOn::SessionData)),
+            this, SLOT(signOnResponse(SignOn::SessionData)),
+            Qt::UniqueConnection);
+    connect(session, SIGNAL(error(SignOn::Error)),
+            this, SLOT(signOnError(SignOn::Error)),
+            Qt::UniqueConnection);
+
+    session->setProperty("account", QVariant::fromValue<Accounts::Account*>(account));
+    session->setProperty("identity", QVariant::fromValue<SignOn::Identity*>(identity));
+    session->process(SignOn::SessionData(signonSessionData), mechanism);
+}
+
+void VKDataTypeSyncAdaptor::signOnError(const SignOn::Error &error)
+{
+    SignOn::AuthSession *session = qobject_cast<SignOn::AuthSession*>(sender());
+    Accounts::Account *account = session->property("account").value<Accounts::Account*>();
+    SignOn::Identity *identity = session->property("identity").value<SignOn::Identity*>();
+    int accountId = account->id();
+    TRACE(SOCIALD_ERROR,
+            QString(QLatin1String("error: credentials for account with id %1 couldn't be retrieved: %2: %3"))
+          .arg(accountId).arg(error.type()).arg(error.message()));
+
+    // if the error is because credentials have expired, we
+    // set the CredentialsNeedUpdate key.
+    if (error.type() == SignOn::AuthSession::UserInteractionError) {
+        setCredentialsNeedUpdate(account);
+    }
+
+    session->disconnect(this);
+    identity->destroySession(session);
+    identity->deleteLater();
+    account->deleteLater();
+
+    // if we couldn't sign in, we can't sync with this account.
+    setStatus(SocialNetworkSyncAdaptor::Error);
+    decrementSemaphore(accountId);
+}
+
+void VKDataTypeSyncAdaptor::signOnResponse(const SignOn::SessionData &responseData)
+{
+    QVariantMap data;
+    foreach (const QString &key, responseData.propertyNames()) {
+        data.insert(key, responseData.getProperty(key));
+    }
+
+    QString accessToken;
+    SignOn::AuthSession *session = qobject_cast<SignOn::AuthSession*>(sender());
+    Accounts::Account *account = session->property("account").value<Accounts::Account*>();
+    SignOn::Identity *identity = session->property("identity").value<SignOn::Identity*>();
+    int accountId = account->id();
+
+    if (data.contains(QLatin1String("AccessToken"))) {
+        accessToken = data.value(QLatin1String("AccessToken")).toString();
+    } else {
+        TRACE(SOCIALD_INFORMATION,
+                QString(QLatin1String("signon response for account with id %1 contained no oauth token"))
+                .arg(accountId));
+    }
+
+    session->disconnect(this);
+    identity->destroySession(session);
+    identity->deleteLater();
+    account->deleteLater();
+
+    if (!accessToken.isEmpty()) {
+        beginSync(accountId, accessToken); // call the derived-class sync entrypoint.
+    }
+
+    decrementSemaphore(accountId);
+}
